674e84875dd0ffa6b329163c1565c6465201974d
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-06 11:55:06 -0400

    Added Course Class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,7 +1,18 @@
 require 'rspec'
+require 'pry'
 require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
-end
+  it "exists" do 
+    course = Course.new("Calculus", 2)
+    expect(course).to be_instance_of(Course)
+  end
+
+  it "has attributes" do
+    course = Course.new("Calculus", 2)
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq 2
+  end
+end 
+
